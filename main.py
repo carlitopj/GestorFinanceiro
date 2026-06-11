f1af753dfb52fd74fa78a01134626b8fd22121b1
carlitopj <carlitopj@gmail.com>
2026-06-10 22:45:03 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from kivymd.app import MDApp
-from kivy.lang import Builder
-
-class GestorFinanceiroApp(MDApp):
-    def build(self):
-        return Builder.load_file("gestor.kv")
-
-if __name__ == "__main__":
-    GestorFinanceiroApp().run()
